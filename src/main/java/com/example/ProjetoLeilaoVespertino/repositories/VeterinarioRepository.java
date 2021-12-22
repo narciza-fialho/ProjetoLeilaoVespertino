43bd1d8d2d264669b9698f3bc7ab63ff93fdd834
Pedrinho-o-brabo <preitman@itelinfo.com>
2021-12-22 16:34:01 -0400

    add veterinario repository

---
+++ src/main/java/com/example/ProjetoLeilaoVespertino/repositories/VeterinarioRepository.java
@@ -0,0 +1,14 @@
+package com.example.ProjetoLeilaoVespertino.repositories;
+
+
+import com.example.ProjetoLeilaoVespertino.entities.Veterinario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VeterinarioRepository extends JpaRepository<Veterinario, Integer> {
+    List<Veterinario> findByAtivo(Boolean ativo);
+}
+
