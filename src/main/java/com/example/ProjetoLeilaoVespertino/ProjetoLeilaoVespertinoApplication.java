c343cad4ef3ee636fccc44e989301eaeb9a41d2a
narciza-fialho <narciza.fialho@hotmail.com>
2021-12-22 14:44:02 -0400

    Adicionando projeto

---
+++ src/main/java/com/example/ProjetoLeilaoVespertino/ProjetoLeilaoVespertinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoLeilaoVespertino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLeilaoVespertinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLeilaoVespertinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjetoLeilaoVespertino/ProjetoLeilaoVespertinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoLeilaoVespertino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLeilaoVespertinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
