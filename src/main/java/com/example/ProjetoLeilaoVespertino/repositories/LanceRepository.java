01cbed9e92f72b22c964fd754871a9f51e43ac21
narciza-fialho <narciza.fialho@hotmail.com>
2021-12-23 15:02:05 -0400

    Regra de negócio LanceBiz

--- src/main/java/com/example/ProjetoLeilaoVespertino/repositories/LanceRepository.java
+++ src/main/java/com/example/ProjetoLeilaoVespertino/repositories/LanceRepository.java
@@ -1,6 +1,7 @@
 package com.example.ProjetoLeilaoVespertino.repositories;
 
 import com.example.ProjetoLeilaoVespertino.entities.Lance;
+import com.example.ProjetoLeilaoVespertino.entities.Leilao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface LanceRepository extends JpaRepository<Lance, Integer> {
     List<Lance> findByAtivo(Boolean ativo);
+    List<Leilao> findByIdLeilao(Integer id);
 }
