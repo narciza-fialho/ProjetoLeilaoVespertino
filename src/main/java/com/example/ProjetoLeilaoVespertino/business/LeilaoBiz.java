725a40b0cb29e434dc5e84cd6bfebc51bbd88631
Pedrinho-o-brabo <pedrinho22cm@gmail.com>
2021-12-23 16:06:31 -0400

    resolvendo a data

--- src/main/java/com/example/ProjetoLeilaoVespertino/business/LeilaoBiz.java
+++ src/main/java/com/example/ProjetoLeilaoVespertino/business/LeilaoBiz.java
@@ -2,52 +2,59 @@ package com.example.ProjetoLeilaoVespertino.business;
 
 import com.example.ProjetoLeilaoVespertino.entities.Leilao;
 import com.example.ProjetoLeilaoVespertino.repositories.LeilaoRepository;
+import net.bytebuddy.build.Plugin;
 
+import javax.xml.crypto.Data;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
+import java.sql.Date;
 import java.util.List;
 
 public class LeilaoBiz {
     private Leilao leilao;
     private LeilaoRepository leilaoRepository;
     private List<String> erros;
+
     public List<String> getErros() {
         return erros;
     }
 
-    public LeilaoBiz(Leilao leilao, LeilaoRepository leilaoRepository){
-        this.leilao =leilao;
+    public LeilaoBiz(Leilao leilao, LeilaoRepository leilaoRepository) {
+        this.leilao = leilao;
         this.leilaoRepository = leilaoRepository;
         this.erros = new ArrayList<>();
 
     }
-    public Boolean isValid(){
+
+    public Boolean isValid() {
         Boolean resultado = nomeNulo(this.leilao.getNome());
+        resultado = verificadorDeData(this.leilao.getData()) && resultado;
 
         return resultado;
     }
-    public Boolean nomeNulo(String nome){
+
+    public Boolean nomeNulo(String nome) {
         Boolean resultado = nome == null;
-        if(resultado)
-        {
-         erros.add("O nome não pode ser nulo");
+        if (resultado) {
+            erros.add("O nome não pode ser nulo");
         }
-        return resultado;
+        return !resultado;
 
     }
-    public Boolean verificadorDeData(String data){
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate dataVerificada = LocalDate.parse(data, dtf);
-        LocalDate hoje = LocalDate.now();
-        Boolean resultado = dataVerificada.compareTo(hoje) <= 0;
-        if(!resultado)
-        {
-         erros.add("Esse dia ja é passado");
+    public Boolean verificadorDeData(Date data) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date hoje = Date.valueOf(LocalDate.now());
+
+        if ( data.before(hoje)) {
+            erros.add("Esse dia ja é passado");
+            return false;
+        } else {
+            return true;
         }
-        return resultado;
 
     }
 }
