507e4a9035920330d20742598427ff174e7f5b2e
ViniciusPaulaSouza <viniciuspaulasouza@gmail.com>
2022-01-11 16:08:24 -0400

    Teste de AnimalRepository

--- src/test/java/com/example/ProjetoLeilaoVespertino/RepositoryTests/VendedorRepositoryTest.java
+++ src/test/java/com/example/ProjetoLeilaoVespertino/RepositoryTests/VendedorRepositoryTest.java
@@ -1,4 +1,32 @@
 package com.example.ProjetoLeilaoVespertino.RepositoryTests;
 
+import com.example.ProjetoLeilaoVespertino.entities.Lance;
+import com.example.ProjetoLeilaoVespertino.entities.Vendedor;
+import com.example.ProjetoLeilaoVespertino.repositories.VendedorRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 public class VendedorRepositoryTest {
+    @Autowired
+    private VendedorRepository vendedorRepository;
+
+    private boolean expected;
+
+    public VendedorRepositoryTest(){
+        expected = true;
+    }
+
+    @Test
+    public void findByAtivoTest(){
+
+        List<Vendedor> listaDeVendedor = vendedorRepository.findByAtivo(true);
+
+        assertThat(!listaDeVendedor.isEmpty()).isEqualTo(expected);
+    }
 }
